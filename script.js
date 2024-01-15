c3dab389373d2b2a59020148937262077691b1fa
lucenafe <desenvolvedor.lucena@gmail.com>
2024-01-15 20:11:52 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,6 @@
+function checkWeather() {
+    // Adicione aqui a lógica para verificar o clima com JavaScript
+    // Por exemplo, você pode fazer uma chamada de API para obter os dados do clima
+    // e exibir o resultado na página.
+    alert("Lógica de verificação de clima aqui.");
+}
